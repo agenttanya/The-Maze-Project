d41471460c2cce7ae99ff88a3d43a24fbe532bba
agenttanya <agenttanya@126.com>
2018-01-18 10:30:57 +0800

    233

--- automove.h
+++ automove.h
@@ -4,18 +4,13 @@
 
 using namespace std;
 
-class AM_Stack
+struct AM_Stack
 {
-  public:
     AM_Stack();  //构造函数
     int push(coor point);  //入栈
     coor pop();  //出栈
     int getLength();  //长度
     coor get_top();  //栈顶
-    void get_path(int currentx, int currenty, maze m);
-    coor get_base(int currentx, int currenty, maze m);
-    coor get_first(int currentx, int currenty, maze m);
-private:
     coor *base;  //基指针
     coor *top;   //顶指针
     int length;   //栈长度
